d53d63748fdb5ccccee475f1643e64a9b0b573f7
devsko <devsko@outlook.com>
2019-03-20 17:04:02 +0100

    first commit

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Hosting;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WindowsXamlManager.InitializeForCurrentThread())
+            {
+                var data = new ObservableCollection<object>();
+                var list = new ListBox();
+                list.ItemsSource = data;
+                data.Add(new object());
+            }
+        }
+    }
+}
